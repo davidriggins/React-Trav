73590485edc20dfb8fbf5779381215f6d7d572fd
David Riggins <driggins@esri.com>
2023-09-11 12:44:23 -0700

    Component Props

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,9 +1,14 @@
-const Header = () => {
+const Header = ({ title }) => {
     return (
         <header>
-            <h1>Task Tracker</h1>
+            <h1>{title}</h1>
         </header>
     );
+};
+
+// Default prop values
+Header.defaultProps = {
+    title: 'Task Tracker',
 };
 
 export default Header;
