240f134627028dd8da804c2129e885a10bbe2b5a
David Riggins <driggins@esri.com>
2023-09-12 11:17:00 -0700

    showAddTask state

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -4,6 +4,8 @@ import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 
 function App() {
+    const [showAddTask, setShowAddTask] = useState(false);
+
     const [tasks, setTasks] = useState([
         {
             id: 1,
@@ -50,7 +52,8 @@ function App() {
     return (
         <div className='container'>
             <Header />
-            <AddTask onAdd={addTask} />
+            {/* // short way of doing ternary without an else */}
+            {showAddTask && <AddTask onAdd={addTask} />}
             {tasks.length > 0 ? (
                 <Tasks
                     tasks={tasks}
