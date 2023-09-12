80f705a3d9e4d3506576f49165fd541c56f63afa
David Riggins <driggins@esri.com>
2023-09-12 10:01:14 -0700

    Delete Task and prop drilling.

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,33 +1,38 @@
-import { useState } from "react";
-import Header from "./components/Header";
-import Tasks from "./components/Tasks";
+import { useState } from 'react';
+import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 function App() {
     const [tasks, setTasks] = useState([
         {
             id: 1,
-            text: "Doctors Appointment",
-            day: "Feb 5th at 2:30pm",
+            text: 'Doctors Appointment',
+            day: 'Feb 5th at 2:30pm',
             reminder: true,
         },
         {
             id: 2,
-            text: "Meeting at School",
-            day: "Feb 6th at 1:30pm",
+            text: 'Meeting at School',
+            day: 'Feb 6th at 1:30pm',
             reminder: true,
         },
         {
             id: 3,
-            text: "Food Shopping",
-            day: "Feb 5th at 2:30pm",
+            text: 'Food Shopping',
+            day: 'Feb 5th at 2:30pm',
             reminder: false,
         },
     ]);
 
+    // Delete Task
+    const deleteTask = (id) => {
+        setTasks(tasks.filter((task) => task.id !== id));
+    };
+
     return (
-        <div className="container">
+        <div className='container'>
             <Header />
-            <Tasks tasks={tasks} />
+            <Tasks tasks={tasks} onDelete={deleteTask} />
         </div>
     );
 }
